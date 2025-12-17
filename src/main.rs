61d6c48e65d961c2c43354397f77737a3b4bafe8
Lenni707 <gamer_like_you@gmx.de>
2025-12-17 14:53:32 +0100

    first stpes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serde_json::*;
+use std::fs;
+
 fn main() {
-    
+    let json: String = fs::read_to_string("/home/lenni/Downloads/user_data_tiktok.json").expect("wrong path dumbass");
+    let v: Value = serde_json::from_str(&json).expect("didnt work");
+
+    match v {
+        Value::Array(map) => {
+            
+        }
+        Value::Object(map) => {
+
+        }
+        _ => {return}
+    }
 }
