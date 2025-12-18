c8f192c5f460c0a7eddcaa52a4f6dc7ce91d4b3c
Lenni <gamer_like_you@gmx.de>
2025-12-18 17:09:39 +0100

    stuck

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,24 @@ use std::collections::HashSet;
 // > wenn bei alle geguckten videos, mit zeit in watchsessions aufteilen
 
 fn main() {
-    let json: String = fs::read_to_string("/Users/lenni/Downloads/tiktok_data.json").expect("wrong path dumbass");
+    let json: String = fs::read_to_string("tiktok_data.json").expect("wrong path dumbass");
     let v: Value = serde_json::from_str(&json).expect("didnt work");
 
-    println!("{:?}", get_top_keys(v));
+    println!("{:?}", get_top_keys(&v));
+    
+    // ghet nicht ka wrm
+    let watched_vids = v
+        .get("Profile")
+        .and_then(|p| p.get("Profile Map")); // iwie brauche ich goofy as hell closures || für bestimte impls (Kapitel 13 rust buch)
+
+    println!("{:?}", watched_vids)
 }
 
-fn get_top_keys(v: Value) -> HashSet<String> {
+fn get_top_keys(v: &Value) -> HashSet<String> {
     let map = v.as_object().expect("expect so abfuck");
     map.keys().cloned().collect() // .cloned macht aus &String -> String, .collect ist goated und bildet aus igerator (map.keys()), was festes wie HashSet (HashSet wegen Return type)
 }
+
+// fn get_specific_values() -> Vec<_> {
+
+// }
